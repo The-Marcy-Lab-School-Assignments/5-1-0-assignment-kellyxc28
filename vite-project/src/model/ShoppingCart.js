6f4bd0861916aa1b08ba380ae5561d311e3da658
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-09 14:30:19 -0400

    Completed: Shopping Cart - Instances Properties Setup

--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,14 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
-
+  // initially thought it should be [...CartItem]
+  #cartItems = [CartItem];
+  
+  constructor(id) {
+    id = getId();
+    this.id = id;
+    this.#cartItems;
+  }
 }
 
 export default ShoppingCart;
