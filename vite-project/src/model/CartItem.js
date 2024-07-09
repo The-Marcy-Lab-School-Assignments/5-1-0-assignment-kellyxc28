35f92a7ccb55c49854fd9fd7044317349f9e17c5
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-07-09 13:47:29 -0400

    Completed Part 1: Cart Item

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,12 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+  constructor(name, price, id) {
+    id = getId();
+    this.name = name;
+    this.price = price;
+    this.id = id;
+  }
 }
 
 export default CartItem;
